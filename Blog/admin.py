2f8090766dfac193126b977f28d9ac0cc94d0709
Aasis <aasispaudel@26gmail.com>
2024-08-07 22:26:46 +0545

    first commit

--- Blog/admin.py
+++ Blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Tag
+from.models import Author
+from.models import Post
 
 # Register your models here.
+
+# class TagAdmin(admin.ModelAdmin):
+#   list_display = ("caption")
+  
+# class AuthorAdmin(admin.ModelAdmin):
+#   list_display = ("first_name","last_name","email_address")
+  
+class PostAdmin(admin.ModelAdmin):
+  list_filter = ("author","tag","date")
+  list_display =("title","date","author")
+  prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Tag)
+admin.site.register(Author)
+admin.site.register(Post,PostAdmin)
--- Blog/views.py
+++ Blog/views.py
@@ -1,91 +1,30 @@
-from django.shortcuts import render
-from datetime import date
+from django.shortcuts import render,get_object_or_404
+
+from .models import Post
 
 
-# all_posts =[
-#  {
-#     "slug": "hike-in-the-mountains",
-#     "image": "Rara.jpg",
-#     "author": "Aasis",
-#     "date" :date(2024,8,23),
-#     "title": "Rara Lake",
-#     "excerpt":"the best place to spend your holidays! this is the heaven",
-#     "content":"""Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, earum veritatis sequi et beatae!
-    
-#     Lorem ipsum dolor sit amet consectetur, 
-#     adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, 
-#     earum veritatis sequi et beatae!
-    
-#     Lorem ipsum dolor sit amet consectetur, 
-#     adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, 
-#     earum veritatis sequi et beatae!
-#     """,
-    
-#   },
-#   {
-#     "slug": "hike-in-the-mountains",
-#     "image": "Rara.jpg",
-#     "author": "Aasis",
-#     "date" :date(2024,8,23),
-#     "title": "Rara Lake",
-#     "excerpt":"the best place to spend your holidays! this is the heaven",
-#     "content":"""Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, earum veritatis sequi et beatae!
-    
-#     Lorem ipsum dolor sit amet consectetur, 
-#     adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, 
-#     earum veritatis sequi et beatae!
-    
-#     Lorem ipsum dolor sit amet consectetur, 
-#     adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, 
-#     earum veritatis sequi et beatae!
-#     """,
-    
-#   },
-#   {
-#     "slug": "hike-in-the-mountains",
-#     "image": "Rara.jpg",
-#     "author": "Aasis",
-#     "date" :date(2024,8,23),
-#     "title": "Rara Lake",
-#     "excerpt":"the best place to spend your holidays! this is the heaven",
-#     "content":"""Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, earum veritatis sequi et beatae!
-    
-#     Lorem ipsum dolor sit amet consectetur, 
-#     adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, 
-#     earum veritatis sequi et beatae!
-    
-#     Lorem ipsum dolor sit amet consectetur, 
-#     adipisicing elit. Necessitatibus rerum quos, rem non tempore fugiat, 
-#     repudiandae repellat optio dolores dolore repellendus eligendi? Odio nemo voluptates, 
-#     earum veritatis sequi et beatae!
-#     """,
-    
-#   }
-# ]
 
-# def get_date(post):
-#   return post['date']
+
 
 # Create your views here.
 
 def starting_page(request):
-  # sorted_posts = sorted(all_posts,key=get_date) 
-  # latest_post = sorted_posts[-3:]
-  return render(request,"Blog/index.html")
+  latest_post= Post.objects.all().order_by("-date")[:3] 
+  return render(request,"Blog/index.html",{
+    "posts":latest_post
+  })
 
 
 def posts(request):
-  return render(request,"Blog/all-posts.html")
+  all_posts = Post.objects.all().order_by("-date")
+  return render(request,"Blog/all-posts.html",{
+    "all_posts" :all_posts
+  })
 
 def post_detail(request,slug):
-  return render(request, "Blog/post-detail.html")
+  identified_post = get_object_or_404(Post, slug=slug)
+  return render(request, "Blog/post-detail.html",{
+    "post": identified_post
+  })
 
 
